5fcb615b0b68df23525513b151cc08c679399e0a
Peijie You <you_peijie@yahoo.com>
2015-10-05 00:22:25 +0800

    可以调用mbr了

--- trylo/foo.c
+++ trylo/foo.c
@@ -1,5 +1,8 @@
 int foo(int x)
 {
-	void (*puts)(const char *str) = (void *)0x1ff0025c;
-	puts("Hello Kernel!\r\n");
+	void (*puts)(const char *) = (void *)0x1FF0000C;
+
+	puts("FW: Hello kernel\r\n");
+	while (1);
+	return 0;
 }
