d3b2d334b19514366da21cd6fb02f12aeb113bde
Peijie You <you_peijie@yahoo.com>
2015-10-19 23:50:12 +0800

    通过编译了。

--- trylo/load_elf.c
+++ trylo/load_elf.c
@@ -2,16 +2,19 @@
 #include <sys/types.h>
 #define BS 512
 
-
 /* TODO not test */
-u32 le2be(u32 x)
-{
-	x = ((x & 0xff00ff00) >> 8) | ((x & 0x00ff00ff) << 8);
-	x = (x >> 16) | (x << 16);
-	return x;
-}
 
-int main()
+/*
+ * u32 le2be(u32 x)
+ * {
+ *	x = ((x & 0xff00ff00) >> 8
+ *	) | ((x & 0x00ff00ff) << 8);
+ *	x = (x >> 16) | (x << 16);
+ *	return x;
+ * }
+ */
+
+int main(void)
 {
 	/*	sdb2, start from mbr[0x1d5] */
 	void (*puts)(const char *) = (void *)0x1FF0000C;
@@ -23,26 +26,31 @@ int main()
 	
 	/* get second partition  */
 	/* TODO sector = page? */
-	u32 *elf_addr = le2be((u32*)(0x100000 + 0x1D6));
+	/* TODO buffer address */
+	u32 *elf_addr = (u32*) (*((u32 *)0x1001D6));
 	volatile u8 *buffer = (void *) 0x120000;
-	sd_dma_spin_read((u32)buffer, 1, *elf_addr);
+	sd_dma_spin_read((u32)buffer, 2, *elf_addr);
 	
 	/* further optimize! le2be only once */
-	void (*kernel_entry)(void) = (void *)le2be(buffer[6]); /* offset 0x18 */
-	u32 *phoff = (u32 *)le2be(buffer[7]); /* offset 0x1C */
-	u16 phentsize = (u16)le2be(buffer[10]) >> 16; /* offset 0x2A */
-	u16 phnum = (u16)le2be(buffer[11]) & 0xFF; /* offset 0x2E */
+	void (*kernel_entry)(void) = (void *)(*(u32 *)(buffer + 0x18)); /* offset 0x18 */
+	u32 *phoff = *((u32 *)(buffer + 0x1C)); /* offset 0x1C */
+	u16 phentsize = *((u16 *)(buffer + 0x2A)); /* offset 0x2A */
+	u16 phnum = *((u16 *)(buffer + 0x2C)); /* offset 0x2E */
 	
+	/* address of current program header entry */
 	phoff += (u32)buffer;
 	/* TODO alignment issue */
 	for (u8 i = 0; i < phnum; i++)
 	{
-		u32 phe_offset = le2be(phoff[1]);
-		u32 phe_vaddr = le2be(phoff[2]);
-		u32 phe_filesz = le2be(phoff[4]);
+		u32 phe_offset = phoff[1];
+		u32 phe_vaddr = phoff[2];
+		u32 phe_filesz = phoff[4];
 		/* u32 phe_memsz = le2be((u32 *)(phoff + 0x5)); */
-		sd_dma_spin_read(phe_vaddr, /*count*/1, /*offset*/phe_offset + buffer);
-		phoff += phentsize;
+		u8 offset = phe_offset & 511;
+		u32 start_id = phe_offset >> 9;
+		u32 end_id = ((phe_offset + (u32)buffer - 1) >> 9) + 1;
+		sd_dma_spin_read(phe_vaddr - offset, end_id - start_id, phe_offset + buffer);
+		phoff += phentsize >> 2;
 	}
 	/*now entering kernel*/
 	kernel_entry();
