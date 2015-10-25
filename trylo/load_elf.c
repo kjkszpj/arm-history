94b065297297018d661438653ebdf9e71098bb63
Piji You <you_peijie@yahoo.com>
2015-10-25 19:22:34 +0800

    整理代码
    还有一点问题，有空再改。
    太长了真·放不下。

--- trylo/load_elf.c
+++ trylo/load_elf.c
@@ -1,56 +1,53 @@
-/*	little endian to big endian */
+/*
+ *	PLEASE refer to elf format CAREFULLY first!
+ *
+ *	Alignment 4?
+ *	TODO:
+ *	1.	buffer address is 0x120000 and 0x130002, do not load segment here
+ *	2.	assuming ph follow after elf header, and the same time block number is NOT enough!!!
+ *	3.	better load to the buffer and copy to target address
+ */
+
 #include "sys/types.h"
 #define BS 512
 
 int main(void)
 {
-	/*	sdb2, start from mbr[0x1d5] */
+	/*	sdb2, starting sector in mbr[0x1D5] */
 	void (*puts)(const char *) = (void *)0x1FF0000C;
 	int (*sd_dma_spin_read)(u32 pa, u16 count, u32 offset) = (void *)0x1ff00014;
-	//int (*sd_dma_spin_write)(u32 pa, u16 count, u32 offset) = (void *)0x1ff00014;
-	
-	/* Alignment 4? */
-	volatile u8 *buffer = (void *) 0x130002;
 	u32 i;
 
+	puts("HI mbr\r\n");
+
+	volatile u8 *buffer = (void *) 0x130002;
 	sd_dma_spin_read((u32)buffer, 1, 0);
+	u32 elf_addr = *(u32 *)(buffer + 0x1D6);
 
 	/* get second partition */
-	/* TODO sector = page? */
-	/* TODO buffer address, fixed or ...? */
-	/* TODO volatile buffer? */
-	u32 elf_addr = *(u32 *)(buffer + 0x1D6);
-	// so far so good
-
-	/* TODO here assume eh followed by ph */
 	buffer = (void *) 0x120000;
 	sd_dma_spin_read((u32)buffer, 3, elf_addr);
 
-	void (*kernel_entry)(void) = (void *)(*(u32 *)(buffer + 0x18)); /* offset 0x18 */
-	u32 *phoff = *((u32 *)(buffer + 0x1C)); /* offset 0x1C */
-	u16 phentsize = *((u16 *)(buffer + 0x2A)); /* offset 0x2A */
-	u16 phnum = *((u16 *)(buffer + 0x2C)); /* offset 0x2E */
+	void (*kernel_entry)(void) = (void *)(*(u32 *)(buffer + 0x18));
+	u32 *phoff = *((u32 *)(buffer + 0x1C));
+	u16 phentsize = *((u16 *)(buffer + 0x2A));
+	u16 phnum = *((u16 *)(buffer + 0x2C));
 	
-	/* address of current program header entry */
+	/* load according to program header */
 	phoff = (u32 *)(((u32)buffer) + ((u32)phoff));
-	/* TODO alignment issue */
 	for (i = 0; i < phnum; i++)
 	{
-		// puts(".\r\n");
 		u32 phe_offset = phoff[1];
 		u32 phe_vaddr = phoff[2];
 		u32 phe_filesz = phoff[4];
-		/* u32 phe_memsz = le2be((u32 *)(phoff + 0x5)); */
 		u8 offset = phe_offset & 511;
-		/* TODO load to buffer */
 		u32 start_id = phe_offset >> 9;
 		u32 end_id = ((phe_offset + (u32)phe_filesz - 1) >> 9) + 1;
-		sd_dma_spin_read(phe_vaddr - offset, end_id - start_id, (phe_offset >> 9) + elf_addr);
+		sd_dma_spin_read(phe_vaddr - offset, end_id - start_id, start_id + elf_addr);
 		phoff += phentsize >> 2;
 	}
 	
 	/*now entering kernel*/
-	puts("MBR!\r\n");
 	kernel_entry();
 	while (1);
 }
