c90f33067e3aebfb28e7215438d51463c2d17974
Piji You <you_peijie@yahoo.com>
2015-11-25 23:13:10 +0800

    start coding slb

--- include/kern/mm/slb.h
+++ include/kern/mm/slb.h
@@ -7,5 +7,25 @@
 
 #include <config.h>
 
+//  TODO slab / silly b / si loud b
+
+struct slb_cache_t
+{
+    slb_cache_t* next;
+};
+
+struct slb_pool_t
+{
+    u32 cnt_used;
+
+    u32 unit_align;
+    u32 unit_size;
+
+    u32 pool_base;
+    u32 pool_size;
+
+    slb_pool_t* next_pool;
+    slb_cache_t* first_cache;
+};
 
 #endif //AIMV6_SLB_H
--- kern/mm/slb.c
+++ kern/mm/slb.c
@@ -2,4 +2,4 @@
 // Created by Peijie You on 15/11/25.
 //
 
-#include "slb.h"
+#include <kern/mm/slb.h>
