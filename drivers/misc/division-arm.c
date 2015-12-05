bc3e0a91dcb9045536babfd6459226fcb69a52c0
Piji You <you_peijie@yahoo.com>
2015-12-05 14:35:28 +0800

    division module
    1.	aim at following c99/c++0x? "round to zero"
    2.	no sure about the call between them
    3.	div0 not fully handled
    4.	explain the idivmod design
    5.	see division-arm.c for more

--- drivers/misc/division-arm.c
+++ drivers/misc/division-arm.c
@@ -13,52 +13,110 @@
  * These functions should not be called directly.
  */
 
-unsigned __aeabi_uidivmod(unsigned numerator, unsigned denominator)
+
+/*
+ * TODO
+ * by Piji You on 15/12/5.
+ * design:
+ * 1.	udivmod, follow the origin implementation, fix for overflow issue
+ * 2.	udiv, call udivmod & operation on registers r0, r1, not sure if it works
+ * 3.	idivmod, see the function, round to zero
+ * 4.	idiv, call idivmod
+ * 5.	idiv0, not fully handled, how to call it?
+ * 6.	need uidiv0?
+ * 7.	64bit div, don't give a shit
+ */
+
+signed int __aeabi_idiv0(signed int numerator, signed int denominator);
+inline unsigned int ABS(signed int x) {return x < 0 ? -x : x;}
+
+unsigned int __aeabi_uidivmod(unsigned int numerator, unsigned int denominator)
 {
-	unsigned tmp = 1;
-	unsigned shift = denominator << 1;
-	unsigned quot = 0;
-	/* TODO handle zero denominator */
-	while (shift <= numerator) {
-		denominator <<= 1;
+//	unsigned int tmp = 1;
+//	unsigned int shift = denominator << 1;
+//	unsigned int quot = 0;
+//	while (shift <= numerator) {
+//		denominator <<= 1;
+//		shift <<= 1;
+//		tmp <<= 1;
+//	}
+//	while (tmp > 0) {
+//		if (numerator > denominator){
+//			quot += tmp;
+//			numerator -= denominator;
+//		}
+//		denominator >>= 1;
+//		tmp >>= 1;
+//	}
+//	asm volatile (
+//		"mov r1, %[rem]"::[rem] "r" (numerator)
+//	);
+//	return quot;
+
+	unsigned int i = 1;
+	unsigned int shift = denominator << 1;
+	unsigned int quot = 0;
+
+	if (denominator == 0) return __aeabi_idiv0(numerator, denominator);
+//	update, prevent shift overflow
+	for (i = 1; shift < 0x80000000 && shift <= numerator; i <<= 1, denominator <<= 1)
 		shift <<= 1;
-		tmp <<= 1;
-	}
-	while (tmp > 0) {
-		if (numerator > denominator){
-			quot += tmp;
+
+	for (; i > 0; i >>= 1, denominator >>= 1)
+		while (numerator >= denominator)
+		{
 			numerator -= denominator;
-		}
-		denominator >>= 1;
-		tmp >>= 1;
-	}
-	asm volatile (
-		"mov r1, %[rem]"::[rem] "r" (numerator)
+			quot += i;
+		};
+
+	asm volatile
+	(
+		"mov r0, %0\n"
+		"mov r1, %1\n"
+		:
+		:"r"(quot), "r"(numerator)
 	);
 	return quot;
 }
 
-unsigned __aeabi_uidiv(unsigned numerator, unsigned denominator)
+unsigned int __aeabi_uidiv(unsigned int numerator, unsigned int denominator)
 {
-	// TODO
-	// asm?
+//	is it ok?
+	return __aeabi_uidivmod(numerator, denominator);
+}
+
+signed int __aeabi_idivmod(signed int numerator, signed int denominator)
+{
+//	result should round to zero? c99, c++0x
+//	-2147483648 / -1 = -2147483648
+
+	if (denominator == 0) return __aeabi_idiv0(numerator, denominator);
+
+	signed int result = __aeabi_uidivmod(ABS(numerator), ABS(denominator));
+
+	if (numerator >= 0 && denominator > 0);
+	if (numerator < 0 && denominator > 0) result = -result;
+	if (numerator >= 0 && denominator < 0) result = -result;
+	if (numerator < 0 && denominator < 0);
+
+	signed int remain = numerator - result * denominator;
+	asm volatile
+	(
+		"mov r0, %0\n"
+		"mov r1, %1\n"
+		:
+		:"r"(result), "r"(remain)
+	);
 	return 0;
 }
 
-unsigned __aeabi_idivmod(unsigned numerator, unsigned denominator)
+signed int __aeabi_idiv(signed int numerator, signed int denominator)
 {
-	// asm?
-	return 0;
+	return __aeabi_idivmod(numerator, denominator);
 }
 
-unsigned __aeabi_idiv(unsigned numerator, unsigned denominator)
-{
-	return 0;
-}
-
-// exception
-// TODO should also implement uidiv0?
-unsigned __aeabi_idiv0(unsigned numerator, unsigned denominator)
+// exception idiv0, should also implement uidiv0?
+signed int __aeabi_idiv0(signed int numerator, signed int denominator)
 {
 	return -1;
 }
