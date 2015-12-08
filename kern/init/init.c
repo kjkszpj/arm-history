9485f79f0f6432cd7fecf1a2beb289e5b6153014
Piji You <you_peijie@yahoo.com>
2015-12-08 11:16:42 +0800

    data abort, no idea how to return from evens.
    弃疗
    接下来做梵语, CV, fuck.

--- kern/init/init.c
+++ kern/init/init.c
@@ -10,11 +10,12 @@
 
 u32 *page_table = (u32*)(KERNEL_BASE + PT_OFFSET);
 
-// in fact, it is more like arch_init()
+// TODO, in fact, it is more like arch_init()
+// DEBUG
+static int print_cpu();
+
 void kinit()
 {
-//    TODO, why this?
-
     uart_spin_puts("GE\r\n\0");
     if (mmu_high_main() == 0) uart_spin_puts("MMU done.\r\n\0");
     if (l1cache_init() == 0) uart_spin_puts("L1 cache done.\r\n\0");
@@ -23,9 +24,11 @@ void kinit()
     if (init_pspace() == 0) uart_spin_puts("pages manage done.\r\n\0");
     if (slb_init() == 0) uart_spin_puts("slb manage done.\r\n\0");
 
-    if (interrupt_init() == 0) uart_spin_puts("int done.\r\n\0"); else uart_spin_puts("bad.\r\n\0");
+    if (interrupt_init() == 0) uart_spin_puts("int done.\r\n\0");
 
-    uart_spin_puts("now triny interrupt\r\n\0");
+    uart_spin_puts("------DEBUG------\r\n\0");
+    print_cpu();
+    uart_spin_puts("now trying interrupt\r\n\0");
     asm volatile
     (
         "SVC #2"
@@ -33,9 +36,12 @@ void kinit()
         :
         :
     );
+//    align, data abort.
+    u32 *b = 0x2;
+    *b = 123;
 
+    print_cpu();
     uart_spin_puts("now trying snprintf\r\n\0");
-
     char temp[100];
     snprintf(temp, 90, "decimal:\t%d %d\r\n\0", 123, 0xFFFD0FA0);
     uart_spin_puts(temp);
@@ -47,9 +53,43 @@ void kinit()
     while (1);
 }
 
+static int print_cpu()
+{
+    u32 a;
+
+    uart_spin_puts("cpu\r\ncpsr:\t\0");
+    asm volatile("MRS r0, cpsr\nMOV %0, r0\n" :"=r"(a) : :"r0");
+    puthex(a);
+
+    uart_spin_puts("spsr:\t\0");
+    asm volatile("MRS r0, spsr\nMOV %0, r0\n" :"=r"(a) : :"r0");
+    puthex(a);
+
+    uart_spin_puts("sp:\t\0");
+    asm volatile("MOV %0, sp\n" :"=r"(a) : :);
+    puthex(a);
+
+    uart_spin_puts("lr:\t\0");
+    asm volatile("MOV %0, lr\n" :"=r"(a) : :);
+    puthex(a);
+
+    uart_spin_puts("r0:\t\0");
+    asm volatile("MOV %0, r0\n" :"=r"(a) : :);
+    puthex(a);
+    uart_spin_puts("r1:\t\0");
+    asm volatile("MOV %0, r1\n" :"=r"(a) : :);
+    puthex(a);
+    uart_spin_puts("r2:\t\0");
+    asm volatile("MOV %0, r2\n" :"=r"(a) : :);
+    puthex(a);
+    return 0;
+}
+
+// DEBUG
 int inte()
 {
     // stack issue?
     uart_spin_puts("successful int\r\n\0");
+    print_cpu();
     return 0;
 }
